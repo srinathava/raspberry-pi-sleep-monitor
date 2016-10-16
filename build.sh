fd9c618f572d06c6342fbc7d4dbc5c1217019d3d
Srinath Avadhanula <srinathava@gmail.com>
2016-10-16 17:42:58 +0000

    polish build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 mkdir -p ~/3p
 cd ~/3p
 
-sudo aptitude install libmicrohttpd-dev libjansson-dev \
+sudo apt-get install -y python-imaging python-twisted
+
+sudo aptitude install -y libmicrohttpd-dev libjansson-dev \
 	libnice-dev libssl-dev libsrtp-dev libsofia-sip-ua-dev \
 	libglib2.0-dev libopus-dev libogg-dev libini-config-dev \
 	libcollection-dev pkg-config gengetopt libtool automake dh-autoreconf
