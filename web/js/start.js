0df5b3f3994874afbd61bbfef859eac38df565d2
Srinath Avadhanula <srinathava@gmail.com>
2017-09-11 18:39:17 -0400

    Append current IP address to try in start.html

--- web/js/start.js
+++ web/js/start.js
@@ -1,6 +1,7 @@
 $(function searchForIp() {
     function tryIp(ipIdx) {
         var uri = "http://192.168.1." + ipIdx;
+        $('#searchStatus').innerHTML = 'Pinging ' + uri;
 
         $.ajax({
             url: uri + "/ping",
